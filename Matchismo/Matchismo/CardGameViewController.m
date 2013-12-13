85f33430e023bfceb828c3ad569e847b1811ac03
Jamar Parris <jamar@jamarparris.com>
2013-12-13 18:48:18 -0500

    Completed Assignment 1.

--- Matchismo/Matchismo/CardGameViewController.m
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -7,13 +7,24 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
 
 @interface CardGameViewController ()
+
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (strong, nonatomic) Deck *deck;
+
 @end
 
 @implementation CardGameViewController
+
+- (Deck *)deck
+{
+    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
+    return _deck;
+}
 
 - (void)setFlipCount:(int)flipCount
 {
@@ -24,20 +35,27 @@
 
 - (IBAction)touchCardButton:(UIButton *)sender
 {
-    
     if ([sender.currentTitle length]) {
         [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
                           forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
     } else {
+        
+        Card *randomCard = [self.deck drawRandomCard];
+        
+        if (!randomCard) {
+            //no more cards so remove button entirely
+            [sender removeFromSuperview];
+            return;
+        }
+        
         [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
                           forState:UIControlStateNormal];
-        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
+        
+        [sender setTitle:[randomCard contents] forState:UIControlStateNormal];
     }
     
     self.flipCount++;
-    
-    
 }
 
 @end
--- Matchismo/Matchismo/Model/PlayingCardDeck.m
+++ Matchismo/Matchismo/Model/PlayingCardDeck.m
@@ -7,6 +7,7 @@
 //
 
 #import "PlayingCardDeck.h"
+#import "PlayingCard.h"
 
 @implementation PlayingCardDeck
 
@@ -15,7 +16,14 @@
     self = [super init];
     
     if (self) {
-        
+        for (NSString *suit in [PlayingCard validSuits]) {
+            for (NSUInteger rank = 1; rank <= [PlayingCard maxRank]; rank++) {
+                PlayingCard *card = [[PlayingCard alloc] init];
+                card.rank = rank;
+                card.suit = suit;
+                [self addCard:card];
+            }
+        }
     }
     
     return self;
